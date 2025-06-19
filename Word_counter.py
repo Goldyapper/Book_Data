b8c41fb889dcfa622e5e03be7215795642ed6902
Adam Parsons <adam.a.parsons15@gmail.com>
2025-06-19 23:02:53 +0100

    prints how many characers in a txt file

--- Word_counter.py
+++ Word_counter.py
@@ -5,7 +5,10 @@ with open("input.txt", "r", encoding="utf-8") as f:
 # Split text into words using default whitespace separator
 words = text.split()
 
-# Count the number of words
+# Count the number of words and characters
 word_count = len(words)
+character_count = len(text)
+
 
 print(f"Total number of words: {word_count}")
+print(f"Total number of characters: {character_count}")
