848a37b50c245667b6746748494c081ad9911c82
Adam Parsons <adam.a.parsons15@gmail.com>
2025-06-20 22:55:11 +0100

    corrected lines to paraphaps

--- Word_counter.py
+++ Word_counter.py
@@ -2,21 +2,20 @@
 with open("input.txt", "r", encoding="utf-8") as f:
     text = f.read()
 
-line_count = 0
-
 # Split text into words using default whitespace separator
 words = text.split()
 
-# Count the number of lines, words and characters
+# Count the number of , words and characters
 word_count = len(words)
 character_count = len(text)
 
-
+# count the number of paragrpahs
+paragraph_count = 0
 Content_List = text.split("\n")
 for i in Content_List:
     if i:
-        line_count += 1
+        paragraph_count += 1
 
-print(f"Total number of lines: {line_count}")
+print(f"Total number of paragraphs: {paragraph_count}")
 print(f"Total number of words: {word_count}")
 print(f"Total number of characters: {character_count}")
