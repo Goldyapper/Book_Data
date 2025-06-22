8841336669d1bdaf349d187642e2c75284ac55d6
Adam Parsons <adam.a.parsons15@gmail.com>
2025-06-22 21:47:51 +0100

    moving code about for best practice

--- Word_counter.py
+++ Word_counter.py
@@ -1,6 +1,16 @@
 from collections import defaultdict
 import re
 
+def count_words_in_file():
+    
+    word_count = defaultdict(int)
+
+    for word in re.findall(r'\b\w+\b', text.lower()):
+        word_count[word] += 1
+    
+    return dict(word_count)
+
+
 # Read the content of the file
 with open("input.txt", "r", encoding="utf-8") as f:
     text = f.read()
@@ -16,27 +26,15 @@ character_count = len(text)
 paragraphs = [p for p in text.split('\n\n') if p.strip()]
 paragraph_count = len(paragraphs)
 
-
-def count_words_in_file():
-    
-    word_count = defaultdict(int)
-
-    for word in re.findall(r'\b\w+\b', text.lower()):
-        word_count[word] += 1
-    
-    return dict(word_count)
-
 word_counts = count_words_in_file()
 
 # count the number of sentences
 sentence_count = len(re.findall(r'[.!?]', text))
 
-
 sorted_word_counts = sorted(word_counts.items(), key=lambda item: item[1])
 
 for word, count in sorted_word_counts:
     print(f"{word}: {count}")
-
 
 print(f"Total number of paragraphs: {paragraph_count}")
 print(f"Total number of sentences: {sentence_count}")
